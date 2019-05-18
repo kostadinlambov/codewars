29dfc3dbfa6be6357997805585ba3265192ed7b9
Kostadin Lambov <valchak@abv.bg>
2019-05-18 22:36:45 +0200

    Added Kyu6 project

---
+++ Kyu6/src/Kata_02_Help_the_bookseller.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Kata_02_Help_the_bookseller {
+
+    public static void main(String[] args) {
+        String art[] = new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
+        String cd[] = new String[]{"A", "B"};
+
+        String result = stockSummary(art, cd);
+        System.out.println(result);
+    }
+
+    public static String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
+        StringBuilder result = new StringBuilder();
+
+        if (lstOf1stLetter.length == 0 || lstOfArt.length == 0) {
+            return "";
+        }
+
+        for (String letter : lstOf1stLetter) {
+            int count = Arrays.stream(lstOfArt)
+                    .filter(art -> art.startsWith(letter))
+                    .mapToInt(art -> Integer.parseInt(art.split(" ")[1])).sum();
+            result.append(String.format("(%s : %d) - ", letter, count));
+        }
+
+        String resultStr = result.toString();
+
+        return resultStr.substring(0, resultStr.length() - 3);
+    }
+}
---
+++ Kyu6/src/Kata_03_Hidden_Cubic_numbers.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Kata_03_Hidden_Cubic_numbers {
+    public static void main(String[] args) {
+        String s = "0 9026315 -827&()";
+        String s2 = "aqdf& 407 0 1 xyz 153 777.777";
+        String s3 = "QK29 45[&erui";
+
+        System.out.println(isSumOfCubes(s));
+        System.out.println(isSumOfCubes(s2));
+        System.out.println(isSumOfCubes(s3));
+    }
+
+    public static String isSumOfCubes(String s) {
+        StringBuilder result = new StringBuilder();
+        String[] inputArr = s.split("\\s+");
+
+        final int[] sum = {0};
+        for (String token : inputArr) {
+            List<Integer> cubicNumber = isCubicNumber(token);
+            if (cubicNumber.size() > 0) {
+                sum[0] += cubicNumber
+                        .stream()
+                        .mapToInt(x -> x)
+                        .sum();
+
+                cubicNumber.forEach(number -> result.append(number).append(" "));
+            }
+        }
+
+        if (result.length() == 0) {
+            return "Unlucky";
+        } else {
+            return result.append(sum[0]).append(" Lucky").toString();
+        }
+    }
+
+    private static List<Integer> isCubicNumber(String token) {
+        List<Integer> resultList = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("^\\d+$");
+        Matcher matcher = pattern.matcher(token);
+
+        if (matcher.find()) {
+            char[] chars = token.toCharArray();
+
+            int counter = 0;
+            int sum = 0;
+            String currentNum = "";
+
+            for (int i = 0; i < chars.length; i++) {
+                currentNum += chars[i] + "";
+                int digit = chars[i] - '0';
+                sum += Math.pow(digit, 3);
+                counter++;
+
+                if (counter == 3 || i == chars.length - 1) {
+                    counter = 0;
+                    int number = Integer.parseInt(currentNum);
+                    currentNum = "";
+                    if (sum == number) {
+                        resultList.add(number);
+                    }
+                    sum = 0;
+                }
+            }
+        }
+
+        return resultList;
+    }
+}
