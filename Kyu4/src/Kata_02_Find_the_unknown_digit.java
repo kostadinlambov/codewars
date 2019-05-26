7db55473db2c7b2e783cab32e879bc11b9837bd2
Kostadin Lambov <valchak@abv.bg>
2019-05-26 23:30:43 +0200

    Find the unknown digit

---
+++ Kyu4/src/Kata_02_Find_the_unknown_digit.java
@@ -0,0 +1,16 @@
+public class Kata_02_Find_the_unknown_digit {
+    public static void main(String[] args) {
+
+    }
+
+    public static int solveExpression( final String expression ) {
+        int missingDigit = -1;
+
+        //Write code to determine the missing digit or unknown rune
+        //Expression will always be in the form
+        //(number)[opperator](number)=(number)
+        //Unknown digit will not be the same as any other digits used in expression
+
+        return missingDigit;
+    }
+}
