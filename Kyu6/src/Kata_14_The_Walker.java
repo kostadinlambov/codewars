ad3af9fd41917db935718fe4c57efa5e0b9031fa
Kostadin Lambov <valchak@abv.bg>
2019-06-11 23:00:43 +0200

    Added Kata The_Walker

---
+++ Kyu6/src/Kata_14_The_Walker.java
@@ -0,0 +1,17 @@
+public class Kata_14_The_Walker {
+    public static void main(String[] args) {
+//        dotest(12, 20, 18, 45, 30, 60, "[15, 135, 49, 18]");
+//        dotest(15,15,19,50,29,55, "[12, 133, 18, 44]");
+//        dotest(14,25,17,41,35,59, "[20, 129, 41, 57]");
+
+        solve(12, 20, 18, 45, 30, 60);
+        solve(15,15,19,50,29,55);
+        solve(14,25,17,41,35,59);
+    }
+
+    public static int[] solve(int a, int b, int c, int alpha, int beta, int gamma) {
+        // your code
+
+        return null;
+    }
+}
