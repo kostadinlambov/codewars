03a14dbdfb204756e2f0fc8522314b9470c1f4a2
Kostadin Lambov <valchak@abv.bg>
2019-06-14 17:05:07 +0200

    Added Kata CountingDuplicates

---
+++ Kyu6/src/Kata_16_CountingDuplicates.java
@@ -0,0 +1,30 @@
+public class Kata_16_CountingDuplicates {
+    public static void main(String[] args) {
+
+//            assertEquals(0, CountingDuplicates.duplicateCount("abcde"));
+//            assertEquals(1, CountingDuplicates.duplicateCount("abcdea"));
+//            assertEquals(1, CountingDuplicates.duplicateCount("indivisibility"));
+
+//        @Test
+//        public void reallyLongStringContainingDuplicatesReturnsThree() {
+//            String testThousandA = new String(new char[1000]).replace('\0', 'a');
+//            String testHundredB = new String(new char[100]).replace('\0', 'b');
+//            String testTenC = new String(new char[10]).replace('\0', 'c');
+//            String test1CapitalA = new String(new char[1]).replace('\0', 'A');
+//            String test1d = new String(new char[1]).replace('\0', 'd');
+//            String test = test1d + test1CapitalA + testTenC +
+//                    testHundredB + testThousandA;
+//            assertEquals(3, CountingDuplicates.duplicateCount(test));
+
+        System.out.println(duplicateCount("abcde"));
+        System.out.println(duplicateCount("abcdea"));
+        System.out.println(duplicateCount("indivisibility"));
+    }
+
+    public static int duplicateCount(String text) {
+        // Write your code here
+
+        return 0;
+    }
+
+}
