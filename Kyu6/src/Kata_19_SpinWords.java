673ea0115e4dc9fa007e1c1e9196338e2ecea88f
Kostadin Lambov <valchak@abv.bg>
2019-06-23 23:44:24 +0200

    Added SpinWords Kata

---
+++ Kyu6/src/Kata_19_SpinWords.java
@@ -0,0 +1,15 @@
+public class Kata_19_SpinWords {
+    public static void main(String[] args) {
+//        assertEquals("emocleW", new SpinWords().spinWords("Welcome"));
+//        assertEquals("Hey wollef sroirraw", new SpinWords().spinWords("Hey fellow warriors"));
+
+        System.out.println(spinWords("Welcome"));
+        System.out.println(spinWords("Hey fellow warriors"));
+    }
+
+    public static String spinWords(String sentence) {
+        //TODO: Code stuff here
+
+        return null;
+    }
+}
